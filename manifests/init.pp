90c42100d1b12f8845b428023936943d95973f99
Kurt Hurtado <github@kurtado.com>
2014-05-27 14:38:52 -0700

    bump version to kibana 3.1.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class kibana3 {
   $dlcmd = "curl -o"
-  $kibana_gz = "https://download.elasticsearch.org/kibana/kibana/kibana-3.0.0.tar.gz"
+#  $kibana_gz = "https://download.elasticsearch.org/kibana/kibana/kibana-3.0.0.tar.gz"
+  $kibana_gz = "https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz"
   $kibana_dir = "/usr/share/kibana"
   $kibana_local = "/root/kibana.tar.gz"
   file { 'kibana_dir':
@@ -18,10 +19,10 @@ class kibana3 {
     command => "tar zxvf ${kibana_local} -C ${kibana_dir} 2> /dev/null",
     path => ['/usr/bin', '/bin'],
     require => File['kibana_dir'],
-    creates => '/usr/share/kibana/kibana-3.0.0/'
+    creates => '/usr/share/kibana/kibana-3.1.0/'
   }
   file { "kibana_config":
-    path => "/usr/share/kibana/kibana-3.0.0/config.js",
+    path => "/usr/share/kibana/kibana-3.1.0/config.js",
     source => "puppet:///modules/kibana3/config.js",
   }
   File['kibana_dir']
